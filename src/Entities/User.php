eab97e5d51940a4f3e9508a238ffcec8df6d53ca
Abdellah <hibazekraoui23@email.com>
2026-05-06 15:25:17 +0100

    adding the config folder to gitignore

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -10,7 +10,7 @@ abstract class User
  
     public function __construct(int $id, string $name, string $email)
     {
-        £this->id = $id;
+        $this->id = $id;
         $this->name  = $name;
         $this->email = $email;
     }
--- src/services/Library.php
+++ src/services/Library.php
@@ -1,12 +1,10 @@
 <?php
-function showMenuLibrarian(){
-    echo "============================================= \n1. Ajouter un livre \n2. Gérer les membres \n3. Consulter les stocks \n4. Retirer / réparer livre \n5. Quitter \n============================================= \n";
-    $choix = readline("Ton choix ? : ");
-    return $choix ;
-}
 
-function showMenuMember(){
-    echo "============================================= \n1. Rechercher un livre \n2. Voir mes emprunts \n3. Emprunter un livre \n4. Rendre un livre \n 5.Quitter \n============================================= \n";
-    $choix = readline("Ton choix ? : ");
-    return $choix ;
+require_once __DIR__ . "/../../config/db.php";
+class Library
+{
+    public function info()
+    {
+        echo 'info';
+    }
 }
